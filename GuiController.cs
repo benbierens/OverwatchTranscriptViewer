b973a127b842adac031c69c0fc6a2c159e45895d
benbierens <thatbenbierens@gmail.com>
2024-07-26 13:26:25 +0200

    open dialog

--- GuiController.cs
+++ GuiController.cs
@@ -3,9 +3,12 @@ using System;
 
 public partial class GuiController : Node
 {
+	private FileDialog fd;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		fd = GetNode<FileDialog>("OpenDialog");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +18,11 @@ public partial class GuiController : Node
 	
 	public void _on_open_button_pressed()
 	{
-		GD.Print("thing!");
+		fd.Visible = true;
 	}
-
+	
+	public void _on_file_selected(string path)
+	{
+		GD.Print("file: " + path);
+	}
 }
